e8b133186d696b7083f372a708db6e2cc8f6fccb
Paul Hebert <paul.hebert@paulhebertdesigns.com>
2017-03-27 17:19:48 -0700

    Lesson 2

--- app/index.js
+++ app/index.js
@@ -1,12 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
-    return (
-      <div>Hello ReactJS Program!</div>
-    )
-  }
-});
+var routes = require('./config/routes');
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(<Avatar user={USER_DATA}/>, document.getElementById('app'));
